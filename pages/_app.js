b0412cc53318081f8a078bcf8e88a38031ee3817
onekill0503 <gamemasteraji123@gmail.com>
2024-04-26 14:08:43 +0700

    feat: add gtag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 function MyApp({ Component, pageProps }) {
   return <>
   <Component {...pageProps} />
   <Analytics />
+  <GoogleTagManager gtmId='G-EDEGNF8SKY' />
 </>
     
 }
